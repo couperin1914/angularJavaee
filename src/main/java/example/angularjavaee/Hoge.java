105d210cbfb84632b6b046bee011827880cbaddf
couperin1914 <couperin1914@yahoo.co.jp>
2018-01-10 23:55:10 +0900

    First commit

---
+++ src/main/java/example/angularjavaee/Hoge.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package example.angularjavaee;
+
+/**
+ * @author hitos
+ *
+ */
+public class Hoge {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
